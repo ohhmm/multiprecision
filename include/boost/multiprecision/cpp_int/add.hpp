67aa82bc358fd736974adf895c4f8f9824cc6d9f
Sergei Krivonos <SergeiKrivonos@gmail.com>
2018-02-05 23:30:46 +0200

    Fixed 0==0 comparision. Add the test case for #43

--- include/boost/multiprecision/cpp_int/add.hpp
+++ include/boost/multiprecision/cpp_int/add.hpp
@@ -266,7 +266,11 @@ eval_decrement(cpp_int_backend<MinBits1, MaxBits1, SignType1, Checked1, Allocato
    if (!result.sign() && result.limbs()[0])
       --result.limbs()[0];
    else if (result.sign() && (result.limbs()[0] < cpp_int_backend<MinBits1, MaxBits1, SignType1, Checked1, Allocator1>::max_limb_value))
+   {
       ++result.limbs()[0];
+	  if(!result.limbs()[0])
+		 result.sign(!result.sign());
+   }
    else
       eval_subtract(result, one);
 }
